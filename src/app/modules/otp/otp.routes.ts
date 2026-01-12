ca42a4a1ef58b61c2121cc48b7930b9e5c2e3005
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-01-12 11:15:41 +0600

    update: multer for form data

--- src/app/modules/otp/otp.routes.ts
+++ src/app/modules/otp/otp.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 import validateRequest from '../../middleware/validateRequest';
 import { otpControllers } from './otp.controller';
 import { resentOtpValidations } from './otp.validation';
 
 const router = Router();
+const upload = multer();
 
 router.post(
   '/verify-otp',
+  upload.none(),
   validateRequest(resentOtpValidations.verifyOtpZodSchema),
   otpControllers.verifyOtp,
 );
