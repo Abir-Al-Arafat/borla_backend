0b68355ea95bd04149aaf9887a8f7b01acb25be8
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-04-04 15:43:40 +0600

    update: validation so that only images can be sent

--- src/app/modules/messages/message.validation.ts
+++ src/app/modules/messages/message.validation.ts
@@ -14,7 +14,7 @@ const sendMessageZodSchema = z.object({
     text: z
       .string()
       .trim()
-      .min(1, 'Message text cannot be empty')
+      .min(0, 'Message text cannot be empty')
       .max(2000, 'Message text cannot exceed 2000 characters')
       .optional(),
   }),
