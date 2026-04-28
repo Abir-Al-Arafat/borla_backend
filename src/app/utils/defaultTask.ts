db2cd2a82827d3705f4722057de6dd9d6531359e
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-04-28 16:17:36 +0600

    update: default task creds

--- src/app/utils/defaultTask.ts
+++ src/app/utils/defaultTask.ts
@@ -3,15 +3,17 @@ import HashPassword from '@app/shared/hashPassword';
 import prisma from '@app/shared/prisma';
 
 export async function defaultTask() {
-  // Add your default task here
-  const email = config?.admin_credentials?.email || 'admin@gmail.com';
-  // check admin is exist
-
+  // check if admin  exists
   const admin = await prisma.user.findFirst({
     where: {
       role: 'admin',
     },
   });
+
+  // Add your default task here
+  const email = config?.admin_credentials?.email || 'admin@email.com';
+  const password = config?.admin_credentials?.password || 'admin@email.com';
+
   if (!admin) {
     await prisma.user.upsert({
       where: { email },
@@ -22,7 +24,7 @@ export async function defaultTask() {
         name: 'MD Admin',
         email,
         phoneNumber: '+8801321834780',
-        password: await HashPassword('112233'),
+        password: await HashPassword(password),
         role: 'admin',
         expireAt: null,
         verification: {
